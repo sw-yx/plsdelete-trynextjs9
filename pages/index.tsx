c6f2be570d9df276e23d3b840dcc82a173747e03
sw-yx <swyx@dontemail.me>
2019-07-09 17:54:01 -0400

    clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
-import React from "react"
+import axios from "axios"
+import Link from "next/link"
+import Head from "next/head"
+import { withRouter, PublicRouterInstance } from "next/router"
 
-type Props = {
-  foo: string
-}
-export default function App({ foo }: Props) {
+type Type = React.FC<{ router: PublicRouterInstance; posts: any }> & { getInitialProps: any }
+const Home: Type = ({ posts }) => {
   return (
     <div>
-      <h1>hello world </h1>
-      <div>
-        this comes from serverless functions: <pre>{foo}</pre>
-      </div>
+      <Head>
+        <title>Next on Netlify</title>
+      </Head>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Link href={`/post?id=${post.id}`} as={`/posts/${post.id}`} passHref>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
 
-App.getInitialProps = () => {
-  return { foo: "David wells is the fkin GOAT" }
+Home.getInitialProps = async (props) => {
+  if (props.res) {
+    console.log("setHeader")
+    props.res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate")
+  }
+
+  console.log(props.asPath, props.query)
+
+  const { data } = await axios.get("https://netlify-json-api.netlify.com/posts")
+
+  return { posts: data }
 }
+export default withRouter(Home)
---
+++ pages/post.tsx
@@ -0,0 +1,44 @@
+import axios from "axios"
+import Link from "next/link"
+import Head from "next/head"
+
+const Post = ({ post }) => {
+  const title = `${post.title} | Next on Netlify`
+  const description = "Next.jsをNetlify上で動かすデモです。"
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://avatars1.githubusercontent.com/u/7007253?s=460&v=4" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@mottox2" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="description" content={description} />
+      </Head>
+      <Link href="/">
+        <a>HOME</a>
+      </Link>
+      <h1>{post.title}</h1>
+      <p>{post.description}</p>
+    </div>
+  )
+}
+
+Post.getInitialProps = async (props) => {
+  if (props.res) {
+    console.log("setHeader")
+    props.res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate")
+  }
+
+  console.log("[renderApp]", props.asPath, props.query, props.req && props.req.query.id)
+  const id = props.req ? props.req.query.id : props.query.id
+
+  const { data } = await axios.get(`https://netlify-json-api.netlify.com/posts/${id}`)
+
+  return { post: data }
+}
+
+export default Post
