688c02a9cf41052ca7124c39e7ca6b5a7eb9d3c9
sw-yx <swyx@dontemail.me>
2019-08-19 10:39:30 -0400

    try testing function

---
+++ functions/testing/testing.js
@@ -0,0 +1,10 @@
+exports.handler = (event, context, callback) => {
+  const firstchunk = event.path.split('/')[1]
+  const renderpath = './' + path.join('./pages', firstchunk === '' ? '/index.js' : firstchunk + '.js')
+  console.log('[render] ', renderpath)
+  console.log({ event })
+  callback(null, {
+    statusCode: 500,
+    body: '200 ok',
+  })
+}
