7a8339bda6a619be888e01f3fc2498c4003a8721
sw-yx <swyx@dontemail.me>
2019-08-19 10:44:51 -0400

    fix path

--- functions/testing/testing.js
+++ functions/testing/testing.js
@@ -1,10 +1,11 @@
+const path = require('path')
 exports.handler = (event, context, callback) => {
   const firstchunk = event.path.split('/')[1]
   const renderpath = './' + path.join('./pages', firstchunk === '' ? '/index.js' : firstchunk + '.js')
   console.log('[render]  ', renderpath)
   console.log({ event })
   callback(null, {
-    statusCode: 500,
+    statusCode: 200,
     body: '200 ok',
   })
 }
